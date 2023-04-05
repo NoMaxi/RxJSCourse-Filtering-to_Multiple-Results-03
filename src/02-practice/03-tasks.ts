f3096f77884aea9ce149224fd6a5bede9c7fe4fa
maksym.novik <maksym.novik@refinitiv.com>
2023-04-05 19:42:04 +0300

    Add practical tasks + propose Task 20_2

--- src/02-practice/03-tasks.ts
+++ src/02-practice/03-tasks.ts
@@ -1,6 +1,6 @@
 import { addItem, run } from './../03-utils';
-import { from, fromEvent, fromEventPattern, generate, interval, of, range, timer, first, last, elementAt, min, max, find, findIndex, single, filter, sample, tap, sampleTime, map, audit, auditTime, throttle, throttleTime, debounce, debounceTime, skip, skipLast, skipUntil, skipWhile, take, takeLast, takeUntil, startWith, takeWhile, distinct, reduce, distinctUntilChanged, distinctUntilKeyChanged, switchMap, withLatestFrom } from 'rxjs';
-import { ajax } from 'rxjs/ajax';
+import { from, fromEvent, fromEventPattern, generate, interval, of, range, timer, first, last, elementAt, min, max, find, findIndex, single, filter, sample, tap, sampleTime, map, audit, auditTime, throttle, throttleTime, debounce, debounceTime, skip, skipLast, skipUntil, skipWhile, take, takeLast, takeUntil, startWith, takeWhile, distinct, reduce, distinctUntilChanged, distinctUntilKeyChanged, switchMap, withLatestFrom, concatMap } from 'rxjs';
+import { ajax, AjaxResponse } from 'rxjs/ajax';
 
 // Task 1. skip()
 // RU: Создайте поток из массива чисел от 1 до 10, используя range()
@@ -8,7 +8,7 @@ import { ajax } from 'rxjs/ajax';
 // EN: Create a stream from an array of numbers from 1 to 10 using range()
 // Get stream elements starting at 3.
 (function task1(): void {
-    // const stream$ = 
+    const stream$ = range(1, 10).pipe(skip(2));
 
     // run(stream$);
 })();
@@ -19,8 +19,8 @@ import { ajax } from 'rxjs/ajax';
 // EN: Create a stream from the array [1, 2, {}] using from()
 // Get the elements of the stream without the last element
 (function task2(): void {
-    // const stream$ = 
-    
+    const stream$ = from([1, 2, {}]).pipe(skipLast(1));
+
     // run(stream$);
 })();
 
@@ -35,7 +35,12 @@ import { ajax } from 'rxjs/ajax';
 // Create a stream of button runBtn click events
 // Ignore the elements of the first stream until the button is clicked
 (function task3(): void {
-    // const stream$ = 
+    const source$ = interval(1000).pipe(
+        tap(value => addItem(value, { color: '#ccc' }))
+    );
+    const btnClicks$ = fromEvent(document.getElementById('runBtn'), 'click');
+
+    const stream$ = source$.pipe(skipUntil(btnClicks$))
 
     // run(stream$);
 })();
@@ -50,7 +55,14 @@ import { ajax } from 'rxjs/ajax';
 // Ignore the elements of the stream as long as they are less than 10, 
 // get 5 elements and complete the stream using take()
 (function task4() {
-    // const stream$ = 
+    const source$ = timer(0, 500).pipe(
+        tap(value => addItem(value, { color: '#ccc' }))
+    );
+
+    const stream$ = source$.pipe(
+        skipWhile(value => value < 10),
+        take(5)
+    )
 
     // run(stream$);
 })();
@@ -62,7 +74,10 @@ import { ajax } from 'rxjs/ajax';
 // EN: Create a stream of button runBtn click events using fromEvent()
 // Get the timestamp of the three clicks using map() and complete the stream.
 (function task5() {
-    // const stream$ = 
+    const stream$ = fromEvent(document.getElementById('runBtn'), 'click').pipe(
+        map(event => event.timeStamp),
+        take(3)
+    );
 
     // run(stream$);
 })();
@@ -75,7 +90,10 @@ import { ajax } from 'rxjs/ajax';
 // Modify the stream to get the last two words in the stream.
 // Collect them into a sentence using reduce()
 (function task6() {
-    // const stream$ = 
+    const stream$ = of('Ignore', 'Ignore', 'Hello', 'World!').pipe(
+        takeLast(2),
+        reduce((acc, value) => `${acc} ${value}`, '')
+    );
 
     // run(stream$);
 })();
@@ -92,7 +110,11 @@ import { ajax } from 'rxjs/ajax';
 // Add the request response to the stream using map().
 // Use the switchMap() operator
 (function task7() {
-    // const stream$ = 
+    const stream$ = timer(0, 1000).pipe(
+        takeUntil(timer(5000)),
+        switchMap(() => ajax('https://api.github.com/users?per_page=5')),
+        map(data => data.response),
+    );
 
     // run(stream$);
 })();
@@ -107,8 +129,17 @@ import { ajax } from 'rxjs/ajax';
 // Get the numbers from the stream as long as they are in the range from 0 to 0.7.
 // Add to the stream also the value that broke the condition.
 (function task81() {
-    //   const stream$ = 
-    
+    function* generator() {
+        while(true) {
+            yield Math.random();
+        }
+    }
+
+    const stream$ = from(generator()).pipe(
+        startWith(0.11),
+        takeWhile(value => value <= 0.7, true)
+    )
+
     // run(stream$);
 })();
 
@@ -120,8 +151,12 @@ import { ajax } from 'rxjs/ajax';
 // Then output the entered string.
 // Use map, reduce
 (function task82() {
-    // const field =    
-    // const stream$ = 
+    const field = document.getElementById('text-field');
+    const stream$ = fromEvent(field, 'keyup').pipe(
+        takeWhile((event: KeyboardEvent) => event.key !== "Enter"),
+        reduce((acc, event) => event),
+        map(event => (event.target as HTMLInputElement).value),
+    )
 
     // run(stream$);
 })();
@@ -134,8 +169,14 @@ import { ajax } from 'rxjs/ajax';
 // Modify the stream so that unique elements remain in the array
 // Use reduce()
 (function task9() {
-    // const stream$ = 
-    
+    const stream$ = from([1, 1, 1, 2, 2, 3, 3, 3, 2, 2, 1, 1]).pipe(
+        distinct(),
+        reduce((acc, elem) => {
+            acc.push(elem);
+            return acc;
+        }, [])
+    );
+
     // run(stream$);
 })();
 
@@ -176,9 +217,16 @@ import { ajax } from 'rxjs/ajax';
                     .add('траве')
                     .add('дрова.');
 
+    const stream$ = generate({
+        initialState: 0,
+        condition: i => i < obj.size,
+        iterate: i => i + 1,
+        resultSelector: i => obj.get(i)
+    }).pipe(
+        distinctUntilChanged(),
+        reduce((acc, word) => `${acc} ${word}`, '')
+    );
 
-    // const stream$ = 
-    
     // run(stream$);
 })();
 
@@ -198,19 +246,19 @@ import { ajax } from 'rxjs/ajax';
             { name: 'Sue' }
         ];
 
-    // const stream$ = 
-    
+    const stream$ = from(ar).pipe(distinctUntilKeyChanged('name'));
+
     // run(stream$);
 })();
 
 
 // Task 12. filter()
 // RU: Пусть есть поток objAddressStream, который выдает объект и второй поток skipFieldsStream, который содержит перечень ключей объекта
-// Необходимо модифицировать поток так, чтобы он выдавал объект без ключей из второго потока. 
+// Необходимо модифицировать поток так, чтобы он выдавал объект без ключей из второго потока.
 // Используйте switchMap, pairs, withLatestFrom, reduce
-// EN: Let there is a stream objAddressStream that produces an object 
+// EN: Let there is a stream objAddressStream that produces an object
 // and a second stream skipFieldsStream that contains a list of the object's keys
-// It is necessary to modify the stream so that it emits an object without keys 
+// It is necessary to modify the stream so that it emits an object without keys
 // from the second stream.
 // Use switchMap, pairs, withLatestFrom, reduce
 (function task12() {
@@ -224,11 +272,24 @@ import { ajax } from 'rxjs/ajax';
     });
 
     const skipFieldsStream$ = from(['build', 'flat']);
-    
-    
-    // const stream$ = 
-    
-    // run(stream$, {outputMethod: 'console'});
+
+    const stream$ = objAddressStream.pipe(
+        switchMap(obj => from(Object.entries(obj))),
+        withLatestFrom(skipFieldsStream$.pipe(
+            reduce((acc, value) => {
+                acc.push(value);
+                return acc;
+            }, [])
+        )),
+        filter(([[key], fields]) => {
+            return fields.indexOf(key) === -1;
+        }),
+        reduce((acc, [[key, value]]) => {
+            return { ...acc, [key]: value };
+        }, [])
+    );
+
+    // run(stream$, { outputMethod: 'console' });
 })();
 
 
@@ -243,8 +304,19 @@ import { ajax } from 'rxjs/ajax';
 // Create a stream of 'click' events on the button using fromEventPattern()
 // Get the last element from the first stream at button click
 (function task13() {
-    // const stream$ = 
-    
+    const btn = document.getElementById('runBtn');
+
+    const addClickHandler = handler => btn.addEventListener('click', handler);
+
+    const removeClickHandler = handler => btn.addEventListener('click', handler);
+
+    const source$ = interval(1000).pipe(
+        tap(value => addItem(value, { color: '#ccc' }))
+    );
+    const btnClicks$ = fromEventPattern(addClickHandler, removeClickHandler);
+
+    const stream$ = source$.pipe(sample(btnClicks$))
+
     // run(stream$);
 })();
 
@@ -258,8 +330,11 @@ import { ajax } from 'rxjs/ajax';
 // Modify this stream so that it emits the last number that was in the stream
 // with a period of 3000ms
 (function task14() {
-    // const stream$ = 
-    
+    const stream$ = interval(1000).pipe(
+        tap(value => addItem(value, { color: '#ccc' })),
+        sampleTime(3000)
+    );
+
     // run(stream$);
 })();
 
@@ -278,8 +353,13 @@ import { ajax } from 'rxjs/ajax';
 // Modify the first stream so that it emits a value only after the time specified in
 // second stream.
 (function task15() {
-    // const stream$ = 
-    
+    const durationSelector = () => interval(1000);
+
+    const stream$ = interval(500).pipe(
+        tap(value => addItem(value, { color: '#ccc' })),
+        audit(durationSelector)
+    );
+
     // run(stream$);
 })();
 
@@ -292,8 +372,11 @@ import { ajax } from 'rxjs/ajax';
 // Print these numbers in gray using tap(), addItem(value, {color: '#ccc'})
 // Modify the first stream so that it only emits numbers every 3s
 (function task16() {
-    // const stream$ = 
-    
+    const stream$ = interval(1000).pipe(
+        tap(value => addItem(value, { color: '#ccc' })),
+        auditTime(3000)
+    );
+
     // run(stream$);
 })();
 
@@ -306,8 +389,11 @@ import { ajax } from 'rxjs/ajax';
 // Print these numbers in gray using tap(), addItem(value, {color: '#ccc'})
 // Modify the first stream so that it emits a number, then emits numbers with a period of number * 1000 ms.
 (function task17() {
-    // const stream$ = 
-    
+    const stream$ = interval(1000).pipe(
+        tap(value => addItem(value, { color: '#ccc' })),
+        throttle(value => interval(value * 1000))
+    );
+
     // run(stream$);
 })();
 
@@ -318,8 +404,10 @@ import { ajax } from 'rxjs/ajax';
 // EN: Create a stream of pointermove events. Modify this stream so that it returns the first value,
 // and then gave a value every 2s
 (function task18() {
-    // const stream$ = 
-    
+    const stream$ = fromEvent(document, 'pointermove').pipe(
+        throttleTime(2000)
+    );
+
     // run(stream$, { outputMethod: 'console'});
 })();
 
@@ -330,8 +418,10 @@ import { ajax } from 'rxjs/ajax';
 // the object will not appear in the stream during the time specified with the the second stream, 
 // for example 500ms.
 (function task19() {
-    // const stream$ = 
-    
+    const stream$ = fromEvent(document, 'pointermove').pipe(
+       debounce(() => interval(500))
+    );
+
     // run(stream$, { outputMethod: 'console'});
 })();
 
@@ -343,9 +433,93 @@ import { ajax } from 'rxjs/ajax';
 // Modify this stream so that it emits the value of the input field after
 // a new value will not appear in the stream for 500ms.
 (function task20() {
-    // const stream$ = 
-    
+    const stream$ = fromEvent(document.getElementById('text-field'), 'keyup').pipe(
+        map(event => (event.target as HTMLInputElement).value),
+        debounceTime(500)
+    )
+
     // run(stream$);
+})();
+
+// Task 20.2. debounceTime()
+// RU: Создайте поток событий по вводу категорий продуктов в текстовое поле с id='text-field', используя fromEvent().
+// Модифицируйте этот поток так, чтобы он выдавал значение поля ввода после того,
+// как в потоке не будет появляться новое значение в течении 1000мс.
+// После получения значения поля ввода должен отправляться запрос
+// ajax('https://dummyjson.com/products/category/${value}'), где ${value} - значение текстового поля.
+// Выведите все продукты из введённой категории, у которых рейтинг выше 4.5.
+// Если категория либо продукт не найдены, выведите сообщение 'Продукты не найдены'.
+// EN: Create a stream of events for entering product categories into the text field with id='text-field' using fromEvent().
+// Modify this stream so that it emits the value of the input field after
+// how a new value will not appear in the stream for 1000ms.
+// After receiving the value of the input field, a request
+// ajax(`https://dummyjson.com/products/category/${value}`) should be sent,
+// where ${value} is the value of the text field.
+// Display all products from the entered category that have a rating higher than 4.5.
+// If the category or product is not found, display the message 'Products not found'.
+(function task20_2() {
+    type Category =
+        'smartphones' |
+        'laptops' |
+        'fragrances' |
+        'skincare' |
+        'groceries' |
+        'home-decoration' |
+        'furniture' |
+        'tops' |
+        'womens-dresses' |
+        'womens-shoes' |
+        'mens-shirts' |
+        'mens-shoes' |
+        'mens-watches' |
+        'womens-watches' |
+        'womens-bags' |
+        'womens-jewellery' |
+        'sunglasses' |
+        'automotive' |
+        'motorcycle' |
+        'lighting';
+    type Product = {
+        id: number,
+        title: string,
+        description: string,
+        price: number,
+        discountPercentage: number,
+        rating: number,
+        stock: number,
+        brand: string,
+        category: Category,
+        thumbnail: string,
+        images: string[]
+    };
+    type ProductsResponse = {
+        products: Product[],
+        total: number,
+        skip: number
+        limit: number,
+    };
+
+    const REQUEST_DELAY = 1000;
+    const PRODUCT_MIN_RATING = 4.5;
+    const MESSAGE = 'Products not found';
+
+    const stream$ = fromEvent(document.getElementById('text-field'), 'keyup').pipe(
+        map(event => (event.target as HTMLInputElement).value),
+        debounceTime(REQUEST_DELAY),
+        switchMap((value) => ajax(`https://dummyjson.com/products/category/${value}`)),
+        map((data: AjaxResponse<ProductsResponse>) => data.response.products),
+        concatMap(products => {
+            if (!products.length) {
+                addItem(MESSAGE);
+            }
+            return from(products);
+        }),
+        filter(({ rating }) => rating >= PRODUCT_MIN_RATING),
+        map(({ description, price, title }) => ({ description, price, title })
+        )
+    );
+
+    run(stream$);
 })();
 
 export function runner() {}
